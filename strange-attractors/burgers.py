ec74efa02cca00e18284af7021e71d59f1daf49d
Alain Galvan <hi@alain.xyz>
2017-07-08 21:34:09 -0400

    Implemented burgers

--- strange-attractors/burgers.py
+++ strange-attractors/burgers.py
@@ -1,2 +1,21 @@
-def burgers(num_points=2000):
-  return NotImplemented
+def burgers(num_points=10000, start=[0.5, 0.5, 0.5], factor=250, radius=0.1, a=0.7, b=0.78):
+    """
+    Generates Burger strange attractor.
+    """
+
+    verts = []
+    zp = 0
+    zq = 0
+    xa = start[0]
+    y = start[1]
+    for i in range(0, num_points):
+        x = (1.0 - a) * xa - y * y
+        y = (1.0 + b) * y + xa * y
+        verts.append([
+            x * factor - radius, y * factor - radius, 0,
+            x * factor - radius, y * factor + radius, 0,
+            x * factor + radius, y * factor + radius, 0,
+            x * factor + radius, y * factor - radius, 0
+        ])
+        xa = x
+    return verts
